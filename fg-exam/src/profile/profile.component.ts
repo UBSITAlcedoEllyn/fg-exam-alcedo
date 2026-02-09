a5fe1ede5e23b7536f271476557e006893e2213a
UBSITAlcedoEllyn <20242248@s.ubaguio.edu>
2026-02-09 10:01:04 +0800

    added the profile html

--- fg-exam/src/profile/profile.component.ts
+++ fg-exam/src/profile/profile.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 
+interface User {
+  name: string;
+  email: string;
+  age: number;
+  location: string;
+  bio: string;
+}
+
 @Component({
   selector: 'app-profile',
-  imports: [],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
-
+  user: User = {
+    name: "Ellyn Alcedo",
+    email: "20242248@s.ubaguio.edu",
+    age: 19,
+    location: "Baguio City, Benguet, Philippines",
+    bio: "A second year IT student at the University of bAGUIO !!"
+  };
 }
