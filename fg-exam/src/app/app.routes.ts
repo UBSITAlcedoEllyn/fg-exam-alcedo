e1e9ca86c65469348088add640acf7f1920777ba
UBSITAlcedoEllyn <20242248@s.ubaguio.edu>
2026-02-09 09:37:12 +0800

    fixed the routes for the components

--- fg-exam/src/app/app.routes.ts
+++ fg-exam/src/app/app.routes.ts
@@ -1,6 +1,28 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
+import { AboutComponent } from '../about/about.component';
+import { FeaturesComponent } from '../features/features.component';
+import { HomeComponent } from '../home/home.component';
 
 export const routes: Routes = [
 
-    
+       {
+        path: 'login',
+    component: LoginComponent,
+    },
+
+    {
+        path: 'about',
+        component: AboutComponent,
+    },
+
+    {
+        path: 'featured',
+        component: FeaturesComponent,
+    },
+
+    {
+        path: 'home',
+        component: HomeComponent,
+    }
 ];
