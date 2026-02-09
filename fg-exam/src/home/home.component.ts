0f32f793f04a0bb50e43601dd31a297a297a5ace
UBSITAlcedoEllyn <20242248@s.ubaguio.edu>
2026-02-09 10:13:59 +0800

    created the home page requirements

--- fg-exam/src/home/home.component.ts
+++ fg-exam/src/home/home.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface appInfo {
+ title: string;
+  description: string;
+  WelcomeMessage: string;
+}
+
 @Component({
   selector: 'app-home',
   imports: [],
@@ -7,5 +13,9 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+    appInfo: appInfo = {
+      title: "My FG-Exam for APPDEV",
+      description: "This is my first grading exam output for my APPDEV subject",
+      WelcomeMessage: "Hello welcome to my app. this app will be my output for my first grading examfor my APPDEV subject. I hope you like it and enjoy"
+}
 }
--- fg-exam/src/profile/profile.component.ts
+++ fg-exam/src/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+
+
 interface User {
   name: string;
   email: string;
